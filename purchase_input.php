79a08de89d4cd83f6efaae0e21b6e2df7894863e
Naoki <freeadores@gmail.com>
2021-05-19 12:19:02 +0900

    fourth deploy

--- purchase_input.php
+++ purchase_input.php
@@ -1,4 +1,12 @@
-<?php session_start(); ?>
+<?php
+ session_start();
+ if (!isset($_SESSION['user'])) {
+	$_SESSION['message'] = "購入手続きを行うにはログインしてください。";
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/login_input_email.php';
+	header("Location:" . $url);
+	exit();
+}
+  ?>
 <!DOCTYPE html>
 <html>
 
@@ -11,11 +19,7 @@
 <body>
 
 
-	<?php
-	if (!isset($_SESSION['user'])) {
-		echo "購入手続きを行うにはログインしてください。";
-	} elseif (!empty($_SESSION['product'])) { 
-	?>
+
 		<table>
 			<tr>
 				<td>お名前：</td>
@@ -33,7 +37,7 @@
 		<p>内容をご確認いただき、ご講習を確定してください。</p>
 		<a href="purchase_output.php">購入を確定する</a>
 	<?php
-	}
+	
 	?>
 	
 </body>
