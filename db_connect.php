ba8235805c40d083f7b2dd2af8d3595ff94a997b
kimizukahayata <yongtaijunzhong@gmail.com>
2021-07-10 11:41:41 +0900

    comit

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,11 @@
 <?php
   // データベースユーザ
-  $user = 'yaoya';
-  $password = 'yaoyapass';
+  $user = 'admin';
+  $password = 'adminpass';
   // 利用するデータベース
   $dbName = 'yaoya';
   // MySQLサーバ
-  $host = 'localhost:3306';
+  $host = 'db-mysql.cghwza3ap5uf.us-east-1.rds.amazonaws.com';
   // MySQLのDSN文字列
   $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
   //MySQLデータベースに接続する
